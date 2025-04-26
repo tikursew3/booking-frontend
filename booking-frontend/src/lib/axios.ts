73aa3c3945caf5c43da0db19e4fe9a7b8bf50116
Menelik Tawye <tikursew3@gmail.com>
2025-04-26 13:54:15 -0500

    update axios

--- booking-frontend/src/lib/axios.ts
+++ booking-frontend/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL, // ✅ Your backend base URL
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL = "https://photography-booking-production.up.railway.app",// ✅ Your backend base URL
   withCredentials: false,
 });
 
