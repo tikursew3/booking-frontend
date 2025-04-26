3e13879f269e24af43ce0ddea85974b2d46f7ee0
Menelik Tawye <tikursew3@gmail.com>
2025-04-26 13:01:57 -0500

    update useDecorItems and useServices file

--- booking-frontend/src/hooks/useServices.ts
+++ booking-frontend/src/hooks/useServices.ts
@@ -1,13 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 import api from '@/lib/axios';
-import { PhotographyService } from '../types/types';
+import { PhotographyService } from '@/types/types';
 
 
 export const useServices = () => {
   return useQuery<PhotographyService[]>({
     queryKey: ['services'],
     queryFn: async () => {
-      const response = await api.get<PhotographyService[]>('api/photography-services');
+      const response = await api.get<PhotographyService[]>('/api/photography-services');
       // ✅ Filter only active services
       return response.data.filter((service) => service.active);
     },
