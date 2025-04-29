1236d01519122395d1e4707f105cb542231e39bf
Menelik Tawye <tikursew3@gmail.com>
2025-04-29 12:39:23 -0500

    updated

--- booking-admin-panel/src/pages/index.tsx
+++ booking-admin-panel/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import AdminLayout from "@/components/AdminLayout";
 import { useEffect, useState } from "react";
 import api from "@/lib/axios";
-import { useRouter } from "next/router";
+//import { useRouter } from "next/router";
 import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, CartesianGrid } from "recharts";
 import { DashboardSummary } from "@/types/types";
 import { ServiceBookingData } from "@/types/types";
@@ -28,7 +28,7 @@ const localizer = dateFnsLocalizer({
 
 
 export default function Dashboard() {
-  const router = useRouter();
+  //const router = useRouter();
   const [summary, setSummary] = useState<DashboardSummary | null>(null);
   const [loading, setLoading] = useState(true);
   const [serviceData, setServiceData] = useState<ServiceBookingData[]>([]);
@@ -42,19 +42,9 @@ export default function Dashboard() {
  
 
   // this useEffect is to protect the page
-  useEffect(() => {
-    const checkAuth = async () => {
-      try {
-        await api.get('/api/bookings/dashboard-summary', { withCredentials: true, });
-        // ✅ User is authenticated — do nothing
-      } catch {
-        //  Not authenticated — redirect to login
-        router.push('/admin/login');
-      }
-    };
-
-    checkAuth();
-  }, [router]);
+ // useEffect(() => {
+   // router.push('/admin/login');
+ // }, [router]);
 
 
   useEffect(() => {
