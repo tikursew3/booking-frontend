44bb0e631b26e68afc66f0765f2346a204c78aa0
Menelik Tawye <tikursew3@gmail.com>
2025-05-01 12:28:35 -0500

    axios updated 2

--- booking-admin-panel/src/lib/axios.ts
+++ booking-admin-panel/src/lib/axios.ts
@@ -3,11 +3,9 @@ import axios from "axios";
 //import Router from "next/router"; // Important: use Next.js router manually
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL // Your backend base URL
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL, // Your backend base URL
+  withCredentials: true // Send cookies automatically
 });
-
-// Send cookies automatically
-axios.defaults.withCredentials = true;
 
 /*
 
