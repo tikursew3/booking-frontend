7fb58f661cb74cd65fd63267a356ad5a01200825
Menelik Tawye <tikursew3@gmail.com>
2025-05-01 12:32:26 -0500

    axios updated 3

--- booking-admin-panel/src/lib/axios.ts
+++ booking-admin-panel/src/lib/axios.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL, // Your backend base URL
-  withCredentials: true // Send cookies automatically
+  withCredentials: true, // Send cookies automatically
 });
 
 /*
