8e366e2e73eddb02f625ae34d76be6d5e8db99eb
Menelik Tawye <tikursew3@gmail.com>
2025-05-01 20:28:25 -0500

    side bar fixed5

--- booking-admin-panel/src/components/AdminLayout.tsx
+++ booking-admin-panel/src/components/AdminLayout.tsx
@@ -38,7 +38,7 @@ export default function AdminLayout({ children }: Props) {
               <li key={item.href}>
                 <Link
                   href={item.href}
-                  className={`flex items-center gap-2 px-3 py-2 rounded hover:bg-black-700 ${
+                  className={`flex items-center gap-2 px-3 py-2 rounded hover:bg-gray-700 ${
                     router.pathname === item.href ? "bg-gray-800 font-semibold" : ""
                   }`}
                   title={item.name} // native tooltip
