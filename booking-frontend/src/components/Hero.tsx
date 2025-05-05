67bd4ac24eb69365dc6fe9b081fd62561c5052b4
Menelik Tawye <tikursew3@gmail.com>
2025-05-05 11:19:37 -0500

    header  updated

--- booking-frontend/src/components/Hero.tsx
+++ booking-frontend/src/components/Hero.tsx
@@ -1,16 +1,16 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="relative min-h-screen w-full overflow-hidden">
+    <section className="relative h-[100vh] pt-16 w-full overflow-hidden">
       {/* Full background image */}
       <div className="absolute inset-0 z-0">
         <Image
           src="/common-element.jpg"
           alt="Photography and Decor Background"
           fill
-          style={{ objectFit: 'cover' }}
+          style={{ objectFit: "cover" }}
           priority
         />
       </div>
@@ -18,13 +18,9 @@ export default function Hero() {
       {/* Overlay */}
       <div className="absolute inset-0 bg-gradient-to-b from-transparent via-black/50 to-black/10 z-10"></div>
 
-      
-      
-
       {/* Main content */}
-      
-      <div className="relative z-20 flex flex-col items-center justify-center text-center h-full px-4 pt-32">
-      
+
+      <div className="relative z-20 flex flex-col items-center justify-center text-center h-full px-4 pt-28">
         <h1 className="text-4xl  md:text-5xl font-bold mb-4 text-white">
           We Capture. We Create.
         </h1>
@@ -34,21 +30,21 @@ export default function Hero() {
       </div>
 
       {/* Scrollable Buttons */}
-      <div className="flex flex-col sm:flex-row gap-6 mt-6">
-          <Link
-            href="/photography"
-            className="bg-[#232b6e] hover:bg-blue-700 text-white py-3 px-6 rounded-2xl text-lg shadow-lg transition hover:scale-105"
-          >
-            📸 Photography
-          </Link>
-          <Link
-            href="/decor"
-            className="bg-[#99b50e] hover:bg-yellow-600 text-white py-3 px-6 rounded-2xl text-lg shadow-lg transition hover:scale-105"
-          >
-            🎀 Decor
-          </Link>
-        </div>
+      <div className="absolute bottom-10 left-0 right-0 flex justify-between px-8 z-30">
+        <Link
+          href="/photography"
+          className="bg-[#232b6e] hover:bg-blue-700 text-white py-3 px-6 rounded-2xl text-lg shadow-lg transition transform hover:scale-105"
+        >
+          Photography
+        </Link>
+
+        <Link
+          href="/decor"
+          className="bg-[#99b50e] hover:bg-yellow-600 text-white py-3 px-6 rounded-2xl text-lg shadow-lg transition transform hover:scale-105"
+        >
+          Decor
+        </Link>
+      </div>
     </section>
   );
 }
-   
