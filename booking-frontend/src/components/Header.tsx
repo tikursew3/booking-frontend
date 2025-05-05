1719c6e6d8199de4373e10ef0020e1987c9611f9
Menelik Tawye <tikursew3@gmail.com>
2025-05-05 14:03:32 -0500

    header background changed

--- booking-frontend/src/components/Header.tsx
+++ booking-frontend/src/components/Header.tsx
@@ -17,7 +17,7 @@ export default function Header() {
   return (
     
     
-<header className="w-full z-50 bg-white/30 backdrop-blur-md supports-[backdrop-filter]:bg-white/20 shadow-md">
+<header className="w-full z-50 bg-white/30 backdrop-blur-md supports-[backdrop-filter]:bg-white/20 shadow-md bg-[#a18132]">
 {/* <header className="fixed top-0 left-0 w-full z-50 h-16 bg-white/30 backdrop-blur-md supports-[backdrop-filter]:bg-white/20 shadow-md"> */}
       <div className="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
         <div className="text-2xl font-extrabold text-[#F9F79F] tracking-tight">
@@ -53,7 +53,7 @@ export default function Header() {
 
       {/* Mobile menu */}
       {isOpen && (
-        <div className="md:hidden bg-white/10 backdrop-blur-sm px-4 py-4 shadow rounded-b-2xl">
+        <div className="md:hidden bg-[#a18132] backdrop-blur-sm px-4 py-4 shadow rounded-b-2xl">
           <ul className="flex flex-col space-y-4 text-[#333333]  font-bold text-lg">
             {navLinks.map((link) => (
               <li key={link.href}>
