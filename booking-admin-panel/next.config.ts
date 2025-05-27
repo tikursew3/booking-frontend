8dd0fc97819db0b0df006eb8dc61b90593a786bf
Menelik Tawye <tikursew3@gmail.com>
2025-05-27 12:57:46 -0500

    base path removed

--- booking-admin-panel/next.config.ts
+++ booking-admin-panel/next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-   //  This tells Next.js to serve everything under /admin
-  basePath: "/admin",
+  
+  
 };
 
 export default nextConfig;
